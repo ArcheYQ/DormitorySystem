122e8e5892ec97efd38a81dcf42fdb9d2239f430
ArcheYQ <951342519@qq.com>
2017-12-01 19:01:08 +0800

    1

--- app/src/main/java/com/example/administrator/dormitorysystem/activity/LoginActivity.java
+++ app/src/main/java/com/example/administrator/dormitorysystem/activity/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
     Button buRegistered;
     @Bind(R.id.tv_loginRule)
     TextView tvLoginRule;
-//回来请吃东西
+//1回来请吃东西
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
