52ef27fdc692f3fa7bcb465a92b227a4f8cc5d09
ArcheYQ <951342519@qq.com>
2018-01-31 00:16:02 +0800

    1

--- app/src/main/java/com/example/administrator/dormitorysystem/activity/DetailStudentActivity.java
+++ app/src/main/java/com/example/administrator/dormitorysystem/activity/DetailStudentActivity.java
@@ -4,7 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
+//import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
